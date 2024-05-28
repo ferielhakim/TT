26b87ea7d5e98d2f8a0986b2e7fd5a4c774d57a6
ferielhakim <feriel.hakim@esprit.tn>
2024-05-28 10:19:04 +0100

    Mise à jour du sous-module

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,271 @@
-import Image from "next/image";
+'use client'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, Typography, Grid, TextField, Button, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, MenuItem, Select, FormControl, InputLabel, TablePagination } from '@mui/material';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+interface SearchResult {
+  id: string;
+  name: string;
+  address: string;
+  usage: string;
+  price: number;
+  availability: string;
+  type: string;
+  open247: string;
 }
+
+const equipmentTypes = [
+  'Baignade extérieure',
+  'Bains-douches',
+  'Bibliothèque',
+  'Brumisateur',
+  'Découverte et Initiation',
+  'Lieux de culte',
+  'Mairie d\'arrondissement',
+  'Musée',
+  'Ombrière pérenne',
+  'Ombrière temporaire',
+  'Piscine',
+  'Terrain de boules'
+];
+
+const spaceTypes = [
+  'Bois',
+  'Cimetières',
+  'Cimetières non parisiens',
+  'Décorations sur la voie publique',
+  'Ephémères, partagés, pédagogiques',
+  'Jardinets décoratifs',
+  'Jardins d\'Etat',
+  'Jardins grandes institutions',
+  'Jardins privatifs',
+  'Promenades ouvertes'
+];
+
+const fountainTypes = [
+  'BORNE_FONTAINE',
+  'FONTAINE_2EN1',
+  'FONTAINE_ALBIEN',
+  'FONTAINE_ARCEAU',
+  'FONTAINE_BOIS',
+  'FONTAINE_TOTEM',
+  'FONTNE_WALLACE',
+  'FTNE_MILLENAIRE',
+  'FTNE_PETILLANTE',
+  'FTNE_POING_EAU'
+];
+
+const App: React.FC = () => {
+  const [results, setResults] = useState<SearchResult[]>([]);
+  const [allResults, setAllResults] = useState<SearchResult[]>([]);
+  const [filters, setFilters] = useState({
+    type: '',
+    open247: '',
+    fountainType: '', // Changement : retirer le filtre de type pour les fontaines
+    globalSearch: '',
+    currentDataset: 'espacesVerts'
+  });
+  const [currentPage, setCurrentPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  useEffect(() => {
+    fetchData(filters.currentDataset);
+  }, [filters.currentDataset]);
+
+  const fetchData = (dataset: string) => {
+    let apiUrl = '';
+    switch (dataset) {
+      case 'espacesVerts':
+        apiUrl = 'https://parisdata.opendatasoft.com/api/records/1.0/search/?dataset=ilots-de-fraicheur-espaces-verts-frais&rows=1000';
+        break;
+      case 'fontaines':
+        apiUrl = 'https://parisdata.opendatasoft.com/api/records/1.0/search/?dataset=fontaines-a-boire&rows=1000';
+        break;
+      case 'equipements':
+        apiUrl = 'https://parisdata.opendatasoft.com/api/records/1.0/search/?dataset=ilots-de-fraicheur-equipements-activites&rows=1000';
+        break;
+      default:
+        break;
+    }
+
+    axios.get(apiUrl)
+      .then(response => {
+        const combinedResults: SearchResult[] = response.data.records.map((item: any, index: number) => ({
+          id: index.toString(),
+          name: item.fields.nom || item.fields.libelle,
+          address: `${item.fields['N° Voie Pair'] || ''} ${item.fields['N° Voie Impair'] || ''} ${item.fields.voie || item.fields.adresse_postale || ''}`.trim(),
+          usage: item.fields.type_objet || item.fields.type || '',
+          price: item.fields.payant === 'Oui' ? 1 : 0,
+          availability: item.fields.disponibilite || item.fields.statut_ouverture || item.fields.horaires_periode || '',
+          type: item.fields.type,
+          open247: item.fields.statut_ouverture === '24/24' ? 'Oui' : 'Non'
+        }));
+        setAllResults(combinedResults);
+        setResults(combinedResults);
+        setCurrentPage(0); // Reset page to 0 whenever new data is fetched
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des données:', error);
+      });
+  };
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<{ name?: string; value: unknown; }>) => {
+    const { name, value } = event.target as HTMLInputElement;
+    setFilters({ ...filters, [name]: value });
+  };
+
+  const handleSearch = () => {
+    const globalSearchLower = filters.globalSearch.toLowerCase();
+    const newFilteredResults = allResults.filter(result => {
+      return (
+        (!filters.type || (result.type && result.type.includes(filters.type))) &&
+        (!filters.equipmentType || result.type === filters.equipmentType) && // Modification: Remplacer filters.equipmentType par filters.fountainType
+        (!filters.fountainType || result.type === filters.fountainType) && // Ajout du filtre pour le type de fontaine
+        (!filters.open247 || result.open247 === filters.open247) &&
+        (globalSearchLower === '' ||
+          (result.name && result.name.toLowerCase().includes(globalSearchLower)) ||
+          (result.address && result.address.toLowerCase().includes(globalSearchLower)) ||
+          (result.usage && result.usage.toLowerCase().includes(globalSearchLower)) ||
+          (result.availability && result.availability.toLowerCase().includes(globalSearchLower)) ||
+          (result.type && result.type.toLowerCase().includes(globalSearchLower)))
+      );
+    });
+    setResults(newFilteredResults);
+    setCurrentPage(0); // Reset page to 0 whenever a new search is performed
+  };
+  
+
+  const handleDatasetToggle = (dataset: string) => {
+    setFilters({ ...filters, currentDataset: dataset });
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setCurrentPage(0);
+  };
+
+  const paginatedResults = results.slice(currentPage * rowsPerPage, currentPage * rowsPerPage + rowsPerPage);
+
+  return (
+    <Container style={{ backgroundColor: '#F5F5F5', color: '#000' }}>
+      <Typography variant="h1" align="center" style={{ fontFamily: 'Nexa', color: '#5f259f' }}>
+        Trouver un endroit frais
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <TextField
+            name="globalSearch"
+            label="Recherche générale"
+            variant="outlined"
+            fullWidth
+            value={filters.globalSearch}
+            onChange={handleFilterChange}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          {filters.currentDataset !== 'fontaines' && ( // Changement : Afficher le filtre de type uniquement pour les datasets autres que fontaines
+            <FormControl variant="outlined" fullWidth margin="normal">
+              <InputLabel>Type</InputLabel>
+              <Select
+                name="type"
+                value={filters.type}
+                onChange={handleFilterChange}
+                label="Type"
+              >
+                <MenuItem value=""><em>Aucun</em></MenuItem>
+                {filters.currentDataset === 'equipements' ? (
+                  equipmentTypes.map(type => (
+                    <MenuItem key={type} value={type}>{type}</MenuItem>
+                  ))
+                ) : (
+                  spaceTypes.map(type => (
+                    <MenuItem key={type} value={type}>{type}</MenuItem>
+                  ))
+                )}
+              </Select>
+            </FormControl>
+          )}
+{/*           {filters.currentDataset === 'fontaines' && (
+            <FormControl variant="outlined" fullWidth margin="normal">
+              <InputLabel>Type de Fontaine</InputLabel>
+              <Select
+                name="fountainType"
+                value={filters.fountainType}
+                onChange={handleFilterChange}
+                label="Type de Fontaine"
+              >
+                <MenuItem value=""><em>Aucun</em></MenuItem>
+                {fountainTypes.map(type => (
+                  <MenuItem key={type} value={type}>{type}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )} */}
+          {filters.currentDataset === 'espacesVerts' && (
+            <FormControl variant="outlined" fullWidth margin="normal">
+              <InputLabel>OUVERTURE 24-24H</InputLabel>
+              <Select
+                name="open247"
+                value={filters.open247}
+                onChange={handleFilterChange}
+                label="OUVERTURE 24-24H"
+              >
+                <MenuItem value=""><em>Aucun</em></MenuItem>
+                <MenuItem value="Oui">Oui</MenuItem>
+                <MenuItem value="Non">Non</MenuItem>
+              </Select>
+            </FormControl>
+          )}
+          <Button variant="contained" color="primary" fullWidth onClick={handleSearch}>Rechercher</Button>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Button onClick={() => handleDatasetToggle('espacesVerts')}>Afficher les espaces verts</Button>
+          <Button onClick={() => handleDatasetToggle('equipements')}>Afficher les équipements et activités</Button>
+          <Button onClick={() => handleDatasetToggle('fontaines')}>Afficher les fontaines à boire</Button>
+
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Nom</TableCell>
+                  <TableCell>Adresse</TableCell>
+                  <TableCell>Type</TableCell>
+                  <TableCell>Prix</TableCell>
+                  <TableCell>Disponibilité</TableCell>
+                  <TableCell>OUVERTURE 24-24H</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {paginatedResults.map(result => (
+                  <TableRow key={result.id}>
+                    <TableCell>{result.name}</TableCell>
+                    <TableCell>{result.address}</TableCell>
+                    <TableCell>{result.usage}</TableCell>
+                    <TableCell>{result.price}</TableCell>
+                    <TableCell>{result.availability}</TableCell>
+                    <TableCell>{result.open247}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 50]}
+            component="div"
+            count={results.length}
+            rowsPerPage={rowsPerPage}
+            page={currentPage}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default App;
